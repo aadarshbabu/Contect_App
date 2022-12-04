bfd7ebca91372a4303bd6581528dab83dfb97737
Aadarsh <aadarsh.kumar@esq.com>
2022-12-05 00:54:12 +0530

    First App

--- components/Contect.js
+++ components/Contect.js
@@ -1,15 +1,70 @@
-import * as Contact from 'expo-contacts';
+import { Button, FlatList, View, Text } from 'react-native';
+import ContectAPI from '../assets/contect';
+import ContectList from './ContectLists';
+import { useState, useEffect } from 'react';
 
-export default async function ContectAPI() {
-  const { status } = await Contact.requestPermissionsAsync();
 
-  if (status === 'granted') {
+//Render context
+function rendreContect({ item }) {
+  return (
+    <ContectList
+      id={item?.id}
+      first_name={item?.name}
+      phone={item?.phoneNumbers ? item?.phoneNumbers[0]?.number : null}
+      email="NAN"
+    />
+  );
+}
 
-    const { data } = await Contact.getContactsAsync({
-      fields: [Contact.Fields.PhoneNumbers],
-    });
 
-    return data
-  
-  }
-}  
+function Header() {
+  return <Text>Contect List</Text>
+}
+
+function Footer() {
+  return <Text>Footer</Text>
+}
+
+
+
+export default function Contect() {
+
+  const [toggle, setToggle] = useState(false);
+  const [phoneContect, setPhoneContect] = useState([]);
+
+
+  useEffect(() => {
+
+    async function getContect() {
+      const c = await ContectAPI()
+      // console.log(c[46])
+      setPhoneContect(c)
+    }
+    getContect()
+  }, []);
+
+
+
+
+  return (
+    <View style={{ padding: 3 }}>
+
+      <Button
+        title={!toggle ? 'show contect' : "hide contect"}
+        onPress={() => setToggle((prev) => !prev)}
+      />
+
+
+      {toggle && (
+        <FlatList
+          data={phoneContect}
+          renderItem={rendreContect}
+          keyExtractor={(data) => data.id}
+          ListHeaderComponent={Header}
+          ListFooterComponent={Footer}
+
+        />
+      )}
+    </View>
+  );
+}
